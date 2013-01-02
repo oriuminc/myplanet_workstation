91249548f2800b967c244a5629898571f5dc52d9
Patrick Connolly <patrick@myplanetdigital.com>
2013-01-02 12:11:06 -0500

    Added RescueTime recipe.

---
+++ cookbooks-override/myplanet_workstation/recipes/rescuetime.rb
@@ -0,0 +1,4 @@
+dmg_package "RescueTime" do
+  source "https://www.rescuetime.com/installers/RescueTimeInstaller.dmg"
+  checksum "87379afab66589b03efe3a76d657fba9ae5bb0630712e8bc1d80e8cf1cf56387"
+end
