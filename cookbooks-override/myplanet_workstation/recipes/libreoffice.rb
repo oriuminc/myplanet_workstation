ee0273923ac07af45d341cfa70e6be6079bba0c5
Patrick Connolly <patrick@myplanetdigital.com>
2013-01-14 16:20:21 -0500

    Removed custom libreoffice, evernote, and sequelpro recipe in favour of pivotal_workstation recipes.

--- cookbooks-override/myplanet_workstation/recipes/libreoffice.rb
+++
@@ -1,5 +0,0 @@
-dmg_package "LibreOffice" do
-  source "http://download.documentfoundation.org/libreoffice/stable/3.6.4/mac/x86/LibO_3.6.4_MacOS_x86_install_en-US.dmg"
-  checksum "4eb29ef38e009214e920fcd98ba0b4aaf2e4fd1d9e27ef5cb466af7ccaf64f88"
-  action :install
-end
