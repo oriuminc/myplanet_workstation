f3647737e0d5045aedd8f6a8fadccceacf668d83
Patrick Connolly <patrick@myplanetdigital.com>
2013-01-14 16:50:05 -0500

    Set hotkey for TotalTerminal.

--- cookbooks-override/myplanet_workstation/recipes/totalterminal.rb
+++ cookbooks-override/myplanet_workstation/recipes/totalterminal.rb
@@ -5,3 +5,15 @@ dmg_package "TotalTerminal" do
   checksum "2fdb9834bcc4cdb97a337b8cb883c13d4453a40fc14b1c9c63faf1952a0e0197"
   type "pkg"
 end
+
+pivotal_workstation_defaults "Enable double-tap hotkey" do
+  domain 'com.apple.Terminal'
+  key 'TotalTerminalVisorHotKey2Enabled'
+  integer 1
+end
+
+pivotal_workstation_defaults "Set control as double-tap hotkey" do
+  domain 'com.apple.Terminal'
+  key 'TotalTerminalVisorHotKey2Mask'
+  integer 262144
+end
